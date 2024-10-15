e2bdbe85692052c401de7b5245201c4e4128a0fd
seongmin <sungmin990923@naver.com>
2024-10-15 17:09:58 +0900

    1015 채팅기능완료

--- src/main/java/com/example/chat/DTO/ChatRoom.java
+++ src/main/java/com/example/chat/DTO/ChatRoom.java
@@ -1,36 +1,25 @@
 package com.example.chat.DTO;
 
-import com.example.chat.service.ChatService;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.web.socket.WebSocketSession;
+import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.UUID;
 
 @Getter
+@Setter
 public class ChatRoom {
 
         private String roomId;
         private String name;
-        private Set<WebSocketSession> sessions = new HashSet<>();
 
         @Builder
-        public ChatRoom(String roomId, String name) {
-            this.roomId = roomId;
-            this.name = name;
+        public static ChatRoom create(String name) {
+            ChatRoom chatRoom = new ChatRoom();
+            chatRoom.roomId = UUID.randomUUID().toString();
+            chatRoom.name = name;
+            return chatRoom;
         }
 
-        public void handleActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService){
-            if(chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-                sessions.add(session);
-                chatMessage.setMessage(chatMessage.getSender() + " 님이 입장하였습니다.");
-            }
-            sendMessage(chatMessage, chatService);
-        }
-
-        public <T> void sendMessage(T message, ChatService chatService){
-            sessions.parallelStream().forEach(session -> chatService.sendMessage(session, message));
-        }
 
 }
